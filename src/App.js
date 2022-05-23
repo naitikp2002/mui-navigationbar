b58d95a19cade0df86f3317f0bb1e400a691f30c
Naitik Patel <naitik2002patel@gmail.com>
2022-05-23 12:57:40 +0530

    1st Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import DenseAppBar from './DenseAppBar';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router';
+import Home from './comp/Home';
+import Contact from './comp/Contact';
+import About from './comp/About';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='Container'>
+      <DenseAppBar />
+      <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/Home" element={<Home/>}/>
+              <Route path="/About" element={<About/>}/>
+              <Route path="/Contact" element={<Contact/>}/>
+      </Routes>
+  </div>
+</>
   );
 }
 
